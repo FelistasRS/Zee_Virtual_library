a07295ef6d553317f43ed3f9a0eb9fe092b21d97
FelistasRS <112381057+FelistasRS@users.noreply.github.com>
2022-09-30 10:19:44 +0200

    Add files via upload

---
+++ homep.php
@@ -0,0 +1,152 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name= "description" content= "">
+    <link rel="stylesheet" href="style1.css"/>
+
+    <title>New</title>
+
+    
+
+</head>
+<body>
+
+<header>
+
+            
+    <h2>Zee Library</h2>
+    <h3>Bringing adventures anywhere and everywhere</h3>
+
+   
+      <form action="DisplayBooks.php" method= "get">
+    <input type="text" placeholder=" Search for any book here...">
+    <input type="submit" name= "submit">
+</form>
+
+
+<h4>Zee Top 5 Trending books!!!</h4>
+<head>
+
+
+<div class= "container">
+  <div class="card">
+    <div class= "imgBx">
+<a href= "#">
+  <img src="40916679.jpg">
+</a>
+<h1> Good girl's guide to murder </h1>
+<p> 5 years ago a case had shaken up the town of Little Kilton when Andie bell was killed 
+    by her boyfriend Sal Singh who later killed himself. Straight A high schooler Pippa Fitz-Amobi isn't so sure ,
+    when she is given a school project she takes the opportunity to revisit the case and
+    find out what TRULY happened. Not everything is what it seems and not everyone is who 
+    they say they are.
+</p>
+<div class= "star">
+          <a>⭐</a>
+          <a>⭐</a>
+          <a>⭐</a>
+          <a>⭐</a>
+          <a>⭐</a>
+          </div>
+</div>
+</div>
+
+    <div class= "container">
+  <div class="card">
+    <div class= "imgBx">
+<a href= "#">
+  <img src="5981262.jpg">
+</a>
+<h1>Unsticky</h1>
+<p> Poor Grace Reeves is mercilessly dumped on her birthday and well...let's just say another 
+    relationship is the last thing on her mind. But when rich,sexy and older art-dealer Vaughn offers her a deal, 
+    Grace hardly has a choice. But then is that all she's worth? A trophy girlfriend? And whatever
+    happened to love?
+</p>
+<div class= "star">
+          <a>⭐</a>
+          <a>⭐</a>
+          <a>⭐</a>
+          <a>⭐</a>
+        
+          </div>
+          </div>
+</div>
+
+
+<meta name='viewport' content='width=device-width, initial-scale=1'>
+<div class= "container">
+  <div class="card">
+    <div class= "imgBx">
+<a href= "#">
+  <img src="20928707.jpg">
+</a>
+<h1> The Mischievious Mrs.Maxfield  </h1>
+<p>Charlotte Samuels assumes she's going to be stuck waiting tables at Marlow's all her life until her life is turned upside down
+    when she gets a marriage proposal from stranger Brandon Marlow who of cause doesn't love or wants to. But he has to do it for the sake 
+    of his legacy. And Charlotte? Well she is tempted to shout "NO!!" but then thinks of all debts waiting to be paid.
+    She's fun, crazy, smart with a little bit of insanity too. This is definitely one book to read!
+</p>
+<div class= "star">
+          <a>⭐</a>
+          <a>⭐</a>
+          <a>⭐</a>
+          <a>⭐</a>
+          <a>⭐</a>
+          </div>
+          </div>
+</div>
+
+<meta name='viewport' content='width=device-width, initial-scale=1'>
+<div class= "container">
+  <div class="card">
+    <div class= "imgBx">
+<a href= "#">
+  <img src="39603448.jpg">
+</a>
+<h1> The Date</h1>
+<p> Imagine waking up ,blood in your hands, a throbbing headache and YOU CANNOT RECOGNISE YOURSELF!
+    or anyone else for that matter. Something happened to Ali yesterday on her date, but what? She cannot remember.
+    She cannot go to the police either, she does not trust them. Especially after what happened that time. Everything as she 
+    knows it has changed. She trusts no one and recognises no one, including the one trying to hurt her.
+    You're in for a thrilling ride as Ali tries to figure out who's her nemesis and the hell happened that night and where has 
+    her bestfriend disappeared to? 
+</p>
+<div class= "star">
+          <a>⭐</a>
+          <a>⭐</a>
+          <a>⭐</a>
+          <a>⭐</a>
+          <a>⭐</a>
+          </div>
+          </div>
+</div>
+
+<meta name='viewport' content='width=device-width, initial-scale=1'>
+<div class= "container">
+  <div class="card">
+    <div class= "imgBx">
+<a href= "#">
+  <img src="2333140.jpg">
+</a>
+<h1>Tale of Tamari</h1>
+<p>A Zimbabwean original. Tamari is 14, an orpharn and her little brother's only source of comfort. Everybody knows her parents died of AIDS
+    and make no effort to hide it and insult her about it. This is a story of a young girl struggling in an economy that does not 
+    seem to care much about opharns .Tamari shows courage and bravery fighting through everyday challenges as she makes her way into her future.
+</p>
+<div class= "star">
+          <a>⭐</a>
+          <a>⭐</a>
+          <a>⭐</a>
+          <a>⭐</a>
+          <a>⭐</a>
+          </div>
+          </div>
+</div>
+</div>
+</body>
+</head>
+
